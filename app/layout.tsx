f312a7fbf4ea02f81490200f672c6a5ed1ed52af
greyyT <nhannguyen8338@gmail.com>
2023-12-12 01:13:28 +0700

    feat(theme): implement Theme Provider in app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/providers';
 
 const fontSans = FontSans({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -13,7 +14,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={cn('bg-background font-sans antialiased', fontSans.variable)}>{children}</body>
+      <body className={cn('bg-background font-sans antialiased', fontSans.variable)}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem storageKey="greyy-blog-theme">
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
