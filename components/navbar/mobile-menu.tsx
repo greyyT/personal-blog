d864b4d72148307f5d17a7bca2fc62d44fe7c724
greyyT <nhannguyen8338@gmail.com>
2023-12-13 02:33:49 +0700

    refactor: loop through navbarItems defined in constants to render links

--- components/navbar/mobile-menu.tsx
+++ components/navbar/mobile-menu.tsx
@@ -5,6 +5,7 @@ import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/co
 import Link from 'next/link';
 import { Separator } from '../ui/separator';
 import Image from 'next/image';
+import { navbarItems } from '@/lib/constants';
 
 export const MobileMenu = () => {
   return (
@@ -18,25 +19,24 @@ export const MobileMenu = () => {
             <div className="flex justify-center w-full mb-3">
               <Image
                 alt="Nhan Avatar"
-                src="/avatar.jpg"
-                width={120}
-                height={120}
-                className="rounded-full dark:border-none border border-solid border-slate-200 object-cover w-32 h-32"
+                src="/hero.png"
+                width={361}
+                height={288}
+                className="rounded-full dark:border-none border border-solid border-slate-200 object-cover object-center w-32 h-32 mb-1"
               />
             </div>
-            <Link href="/" className="font-semibold text-text-dark dark:text-white text-xl">
+            <Link href="/" className="font-semibold text-text-dark dark:text-white text-xl py-4">
               Nhan Nguyen
             </Link>
           </SheetTitle>
         </SheetHeader>
         <Separator className="my-4" />
         <div className="flex flex-col w-full">
-          <Link href="/blog" className="p-3 hover:bg-slate-100/70 dark:hover:bg-slate-800 rounded-md">
-            Blog
-          </Link>
-          <Link href="/projects" className="p-3 hover:bg-slate-100/70 dark:hover:bg-slate-800 rounded-md">
-            Projects
-          </Link>
+          {navbarItems.map((item, idx) => (
+            <Link key={idx} href={item.href} className="p-3 hover:bg-slate-100/70 dark:hover:bg-slate-800 rounded-md">
+              {item.name}
+            </Link>
+          ))}
         </div>
       </SheetContent>
     </Sheet>
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -3,6 +3,8 @@
 import Link from 'next/link';
 import { ModeToggle } from '@/components/mode-toggle';
 import { MobileMenu } from './mobile-menu';
+import { Code2 } from 'lucide-react';
+import { navbarItems } from '@/lib/constants';
 
 export const Navbar = () => {
   return (
@@ -10,17 +12,17 @@ export const Navbar = () => {
       <div className="w-305 max-w-full mx-5 lg:mx-16 xl:mx-24 2xl:mx-28 h-15 flex justify-between items-center lg:mt-7.5">
         <Link
           href="/"
-          className="font-semibold text-text-dark dark:text-white text-xl lg:text-2xl flex items-center h-full"
+          className="font-semibold text-text-dark dark:text-white text-xl lg:text-2xl flex items-center h-full hover:scale-105 transition"
         >
+          <Code2 className="text-text-secondary w-8 h-8 mr-2" />
           Nhan Nguyen
         </Link>
         <div className="hidden sm:flex gap-[14px] items-center text-text-dark dark:text-white text-lg lg:text-xl">
-          <Link className="p-2" href="/blog">
-            Blog
-          </Link>
-          <Link className="p-2" href="/projects">
-            Projects
-          </Link>
+          {navbarItems.map((item, idx) => (
+            <Link key={idx} className="p-2" href={item.href}>
+              {item.name}
+            </Link>
+          ))}
           <div className="ml-2">
             <ModeToggle />
           </div>
