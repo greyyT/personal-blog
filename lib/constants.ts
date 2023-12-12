124df9b6ebf649f9585ad1206281298f6b407602
greyyT <nhannguyen8338@gmail.com>
2023-12-13 02:31:26 +0700

    feat(util): add constants.ts for storing constants

---
+++ lib/constants.ts
@@ -0,0 +1,39 @@
+import { Github, Linkedin, Mail, Phone } from 'lucide-react';
+
+export const navbarItems = [
+  {
+    name: 'Home',
+    href: '/',
+  },
+  {
+    name: 'Blog',
+    href: '/blog',
+  },
+  {
+    name: 'Projects',
+    href: '/projects',
+  },
+  {
+    name: 'Contact',
+    href: '/contact',
+  },
+];
+
+export const socialMedia = [
+  {
+    href: 'mailto:nhannguyen312003@gmail.com',
+    icon: Mail,
+  },
+  {
+    href: 'tel: +84768040174',
+    icon: Phone,
+  },
+  {
+    href: 'https://github.com/greyyT',
+    icon: Github,
+  },
+  {
+    href: 'https://www.linkedin.com/in/greyyt/',
+    icon: Linkedin,
+  },
+];
