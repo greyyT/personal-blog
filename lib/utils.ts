56f63b0b0342ebc75001b8877ba6e2c3c05f916c
greyyT <nhannguyen8338@gmail.com>
2023-12-12 17:50:17 +0700

    feat(util): add getBlogs for getting all the blogs from docs

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { Doc } from 'contentlayer/generated';
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getBlogs(docs: Doc[]) {
+  return docs.slice().filter((doc) => doc.docType === 'blog');
 }
