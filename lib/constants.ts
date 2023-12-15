615b682c91c134e2d7ea54826dd507c695d7bde4
greyyT <nhannguyen8338@gmail.com>
2023-12-15 22:34:49 +0700

    chore(constants): declare constants for rendering items

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
+import { Aisia, Hcmus } from '@/public/company';
 import { Github, Linkedin, Mail, Phone } from 'lucide-react';
 
 export const navbarItems = [
@@ -25,15 +26,70 @@ export const socialMedia = [
     icon: Mail,
   },
   {
-    href: 'tel: +84768040174',
-    icon: Phone,
-  },
-  {
     href: 'https://github.com/greyyT',
     icon: Github,
   },
   {
     href: 'https://www.linkedin.com/in/greyyt/',
     icon: Linkedin,
+  },
+];
+
+export const experiences = [
+  {
+    title: 'Bachelor Degree in Data Science',
+    company_name: 'University of Science - VNUHCM',
+    icon: Hcmus,
+    iconBg: '#E6DEDD',
+    date: 'Oct 2021 - Present',
+    points: [
+      {
+        content: 'Current GPA: 3.26/4.0',
+        subContent: null,
+      },
+      {
+        content: 'Revelant Coursework:',
+        subContent: [
+          'Databases',
+          'Database Management Systems',
+          'Computer Network',
+          'Data Structures and Algorithms',
+          'Probability and Statistics',
+          'Discrete Math',
+          'Object Oriented Programming',
+        ],
+      },
+      {
+        content: 'Activities:',
+        subContent: ['Former member of the VNU HCMUS Math and Computer Science Academic Club (2021 - 2022).'],
+      },
+    ],
+  },
+  {
+    title: 'Frontend Developer Freelancer',
+    company_name: 'AISIA Research Lab',
+    icon: Aisia,
+    iconBg: '#E6DEDD',
+    date: 'Jan 2023 - March 2023',
+    points: [
+      {
+        content:
+          'Reimplementing the old website written in Wordpress to a new, clean, maintainable website supported by Ruby on Rails template engine and other technologies.',
+        subContent: null,
+      },
+      {
+        content:
+          'Collaborating with cross-functional teams including designers and other developers to create high-quality products.',
+        subContent: null,
+      },
+      {
+        content: 'Implementing responsive design and ensuring cross-browser compatibility.',
+        subContent: null,
+      },
+      {
+        content: 'Participating in code reviews and providing constructive feedback to other developers.',
+        subContent: null,
+      },
+    ],
   },
 ];
